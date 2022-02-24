7a650c3b99052bcdec915140114676d29b31c4e6
Tommaso Amici <tamici@ripe.net>
2022-02-24 17:34:35 +0100

    fix: updated install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux"* ]]; then
 
-    # install packages
-    # git repository
-    sudo add-apt-repository ppa:git-core/ppa
+  # install packages
+  # git repository
+  sudo add-apt-repository ppa:git-core/ppa
 
-    # yarn repo
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  # update packages
+  sudo apt-get update
+  sudo apt-get upgrade -y
+  sudo apt-get dist-upgrade
 
-    # update packages
-    sudo apt-get update
-    sudo apt-get upgrade -y
-    sudo apt-get dist-upgrade
+  sudo apt-get install git python3 python3-pip vim zsh
 
-    sudo apt-get install git python3 python3-pip vim zsh yarn
+  # nodejs
+  curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+  npm install --global yarn
 
-    # nodejs
-    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-    sudo apt-get install -y nodejs
-
-    # ripgrep
-    curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
-    sudo dpkg -i ripgrep_0.8.1_amd64.deb
+  # ripgrep
+  curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
+  sudo dpkg -i ripgrep_0.8.1_amd64.deb
 fi
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    # macOS
-    xcode-select --install
-    # install homebrew
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  # macOS
+  xcode-select --install
+  # install homebrew
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
   brew update
   brew install git vim zsh python yarn node ripgrep
@@ -38,8 +35,6 @@ fi
 # update python packages
 pip install --upgrade setuptools
 pip install --upgrade pip
-
-yarn global add sass
 
 # get dotfiles
 git clone https://github.com/TommasoAmici/dotfiles.git
@@ -51,6 +46,8 @@ ln -s dotfiles/vim/vimrc ~/.vimrc
 
 # set up zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-curl -L git.io/antigen > antigen.zsh
+curl -L git.io/antigen >antigen.zsh
 
 ln -s dotfiles/zshrc ~/.zshrc
+ln -s dotfiles/gitconfig ~/.gitconfig
+ln -s dotfiles/gitconfig-personal ~/.gitconfig-personal
