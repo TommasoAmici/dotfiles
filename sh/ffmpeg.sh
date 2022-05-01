4ea7f083bea2d33133ef341c1c32d6414d6ed2d0
Tommaso Amici <me@tommasoamici.com>
2022-05-01 12:59:37 +0200

    fix(ffmpeg)!: prefixed all functions with ffmpeg_
    BREAKING CHANGE: prepended all functions with ffmpeg_ to make it easier
    to retrieve them by pressing tab in the shell

--- sh/ffmpeg.sh
+++ sh/ffmpeg.sh
@@ -7,19 +7,19 @@
 #   - $2: optional output file name, sane defaults are provided
 
 # Converts given video to mp4
-to_mp4() {
+ffmpeg_to_mp4() {
   OUT="${2:-$1}"
   ffmpeg -i "$1" -q:v 0 "$OUT.mp4"
 }
 
 # Removes sound from given video
-remove_sound() {
+ffmpeg_remove_sound() {
   OUT="${2:-nosound-$1}"
   ffmpeg -i "$1" -c copy -an "$OUT"
 }
 
 # Encodes video with better settings
-optimize_mp4() {
+ffmpeg_optimize_mp4() {
   OUT="${2:-$1-optimized}"
   ffmpeg -i "$1" -vcodec libx264 -crf 28 "$OUT.mp4"
 }
@@ -27,7 +27,7 @@ optimize_mp4() {
 # Extracts a clip of length $2, starting from $1
 # Usage: extract_clip hh:mm:ss hh:mm:ss input [output]
 # Example: extract_clip 00:04:59 00:00:11 input.mp4 [output.mp4]
-extract_clip() {
+ffmpeg_extract_clip() {
   START="$1"
   LENGTH="$2"
   INPUT="$3"
