cdcb2b6d568b536969e6a9298b4dfad7ec65dc6c
Tommaso Amici <me@tommasoamici.com>
2022-06-30 15:25:16 +0200

    feat(sh): added convenience aliases for semgrep

--- sh/index.sh
+++ sh/index.sh
@@ -2,25 +2,28 @@
 
 # This index file appropriately loads all other files from the sh/ directory
 
-# shellcheck source=./sh/log.sh
+# shellcheck source=./log.sh
 [ -f ~/dotfiles/sh/log.sh ] && . ~/dotfiles/sh/log.sh
 
-# shellcheck source=./sh/env.sh
+# shellcheck source=./env.sh
 [ -f ~/dotfiles/sh/env.sh ] && . ~/dotfiles/sh/env.sh
 
-# shellcheck source=./sh/git.sh
+# shellcheck source=./git.sh
 [ -f ~/dotfiles/sh/git.sh ] && . ~/dotfiles/sh/git.sh
 
-# shellcheck source=./sh/random.sh
+# shellcheck source=./random.sh
 [ -f ~/dotfiles/sh/random.sh ] && . ~/dotfiles/sh/random.sh
 
-# shellcheck source=./sh/images.sh
+# shellcheck source=./images.sh
 [ -f ~/dotfiles/sh/images.sh ] && . ~/dotfiles/sh/images.sh
 
-# shellcheck source=./sh/python.sh
+# shellcheck source=./python.sh
 [ -f ~/dotfiles/sh/python.sh ] && . ~/dotfiles/sh/python.sh
 
+# shellcheck source=./semgrep.sh
+[ -f ~/dotfiles/sh/semgrep.sh ] && . ~/dotfiles/sh/semgrep.sh
+
 if type ffmpeg >/dev/null 2>&1; then
-  # shellcheck source=./sh/ffmpeg.sh
+  # shellcheck source=./ffmpeg.sh
   [ -f ~/dotfiles/sh/ffmpeg.sh ] && . ~/dotfiles/sh/ffmpeg.sh
 fi
---
+++ sh/semgrep.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Convenience aliases for semgrep
+# Author: Tommaso Amici
+
+alias semgrep_ci='semgrep --config "p/ci"'
+alias semgrep_python='semgrep --config "p/python"'
+alias semgrep_django='semgrep --config "p/django"'
+alias semgrep_go='semgrep --config "p/golang"'
+alias semgrep_ocaml='semgrep --config "p/ocaml"'
+alias semgrep_js='semgrep --config "p/javascript"'
+alias semgrep_ts='semgrep --config "p/typescript"'
