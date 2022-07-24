6dd76960b32507ab46dde05242cbd0229398b9c6
Tommaso Amici <me@tommasoamici.com>
2022-07-24 13:58:13 +0200

    refactor(python): use --upgrade-deps when creating venv

--- sh/python.sh
+++ sh/python.sh
@@ -11,8 +11,8 @@ alias pir='pip install -r requirements.txt'
 alias pyserve="p -m http.server --bind 127.0.0.1"
 
 venv() {
-  python3 -m venv venv &&
+  python3 -m venv --upgrade-deps venv &&
     . venv/bin/activate &&
-    python -m pip install --upgrade pip wheel &&
+    python -m pip install --upgrade wheel &&
     [ -f requirements.txt ] && python -m pip install -r requirements.txt
 }
